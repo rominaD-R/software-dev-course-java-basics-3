21b3d302664ea437c833ddf43db237c28e6a3f6c
Romina Diaz-Rivero <romina.diazrivero@gmail.com>
2026-05-02 20:56:45 -0500

    did the conditionals and loops

--- src/main/java/org/example/LoopExercises.java
+++ src/main/java/org/example/LoopExercises.java
@@ -4,13 +4,26 @@ public class LoopExercises {
     public int sum(int n) {
         // Replace the line below with code that returns the sum of the numbers from 1 to n
         // (use a for loop)
-        return 0;
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum = sum + i;
+        }
+        return sum;
     }
 
     public int sumUntilEven(int n) {
         // Replace the line below with code that returns the sum of the numbers from 1 to n
         // but stops adding when the sum is even
         // (use a while loop with a sum variable and a counter variable)
-        return 0;
+        int sum = 0;
+        int i = 0;
+        while (i <= n) {
+            sum = sum + i;
+            i++;
+            if (sum % 2 == 0 && sum != 0) {
+                break;
+            }
+        }
+        return sum;
     }
 }
